d84932270970bc426ec7ccd56df04fde6ecbf58b
AtoJ-ManasAnjalekar <manas.atoj@gmail.com>
2022-07-25 17:38:24 +0530

    Level 3 - Using Hashes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -30,7 +30,6 @@ const userSchema = new mongoose.Schema({
     minlength: 5
   }
 });
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model('User', userSchema);
 
@@ -49,7 +48,7 @@ app.get('/register', function(req, res){
 app.post('/register', function(req, res){
   const user = new User( {
     email: req.body.email,
-    password: req.body.password
+    password: md5(req.body.password)
   } );
 
   user.save( function(err){
@@ -64,7 +63,7 @@ app.post('/register', function(req, res){
 app.post('/login', function(req, res){
 
   const email = req.body.email;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne( {email: email}, function(err, user) {
     if(err){
