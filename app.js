98a3131716083e4a7e957e4ec4c1b03dbe19005f
AtoJ-ManasAnjalekar <manas.atoj@gmail.com>
2022-07-25 19:23:37 +0530

    Level 5 - Using Passport to hash and salt user-data

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
 app.use(express.static("public"));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect('mongodb+srv://admin-Manas:AdminPassword@cluster0.i4vdm.mongodb.net/secretDB?retryWrites=true', {
   useNewUrlParser: true,
@@ -20,19 +27,22 @@ mongoose.connect('mongodb+srv://admin-Manas:AdminPassword@cluster0.i4vdm.mongodb
 });
 
 const userSchema = new mongoose.Schema({
-  email: {
+  username: {
     type: String,
-    required: [true, 'Please provide a suitable email!'],
     minlength: 5
   },
   password: {
     type: String,
-    required: [true, 'Please provide a suitable content password!'],
     minlength: 5
   }
 });
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model('User', userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function(req, res){
   res.render('home');
@@ -46,44 +56,51 @@ app.get('/register', function(req, res){
   res.render('register');
 });
 
-app.post('/register', function(req, res){
+app.get('/secrets', function(req, res){
+  if(req.isAuthenticated()){
+    res.render('secrets');
+  }else{
+    res.redirect('/login');
+  }
+});
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const user = new User( {
-      email: req.body.email,
-      password: hash
-    } );
-
-    user.save( function(err){
-      if(err){
-        console.log(err);
-      }else{
-        res.render('secrets');
-      }
-    } );
+app.get('/logout', function(req, res){
+  req.logout(function(err){
+    if(err){
+      console.log(err);
+    }
   });
+  res.redirect('/');
+})
 
-
+app.post('/register', function(req, res){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect('/register');
+    }else{
+      passport.authenticate('local')(req, res, function(){
+        res.redirect('/secrets');
+      });
+    }
+  });
 });
 
 app.post('/login', function(req, res){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
-  const email = req.body.email;
-  const password = req.body.password;
-
-  User.findOne( {email: email}, function(err, user) {
+  req.login(user, function(err) {
     if(err){
       console.log(err);
     }else{
-      if(user){
-        bcrypt.compare(password, user.password, function(err, result) {
-          if(result === true){
-            res.render('secrets');
-          }
-        });
-      }
+      passport.authenticate('local')(req, res, function(){
+        res.redirect('/secrets');
+      });
     }
-  } );
+  });
 });
 
 let port = process.env.PORT;
